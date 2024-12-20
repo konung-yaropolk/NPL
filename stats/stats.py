6bff0c8de34d0a6893fc133041b20c39e7001e55
Yaroslav Andrianov <yaropolk1995@gmail.com>
2024-12-20 05:00:08 -0600

    modified:   stats/stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -1,24 +1,60 @@
-import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
+class __StatisticalTests():
+    '''Statistical tests mixin'''
 
-class StatisticalAnalysis:
-    def __init__(self, data_list, paired=False, tail='two-tailed'):
-        self.data_list = data_list
-        self.paired = paired
-        self.tail = tail
-        self.normals = []
-        self.methods = []
-        self.test_name = None
-        self.test_stat = None
-        self.test_p_value = None
+    def t_test(self):
+        if self.paired:
+            t_stat, t_p_value = stats.ttest_rel(self.data_list[0], self.data_list[1])
+        else:
+            t_stat, t_p_value = stats.ttest_ind(self.data_list[0], self.data_list[1])
+        
+        if self.tails == 1:
+            t_p_value /= 2
+        
+        self.test_name = "t-test"
+        self.test_stat = t_stat
+        self.test_p_value = t_p_value
 
-        for data in data_list:
-            normal, method = self.check_normality(data)
-            self.normals.append(normal)
-            self.methods.append(method)
-            print(f"Data is {'normal' if normal else 'not normal'} (checked by {method})")
+    def mann_whitney_u_test(self):
+        stat, p_value = stats.mannwhitneyu(self.data_list[0], self.data_list[1], alternative='two-sided' if self.tails == 2 else 'greater')
+        self.test_name = "Mann-Whitney U test"
+        self.test_stat = stat
+        self.test_p_value = p_value
+
+    def wilcoxon_signed_rank_test(self):
+        stat, p_value = stats.wilcoxon(self.data_list[0], self.data_list[1])
+        if self.tails == 1 and p_value > 0.5:
+            p_value = 1 - p_value
+        self.test_name = "Wilcoxon signed-rank test"
+        self.test_stat = stat
+        self.test_p_value = p_value
+
+    def anova(self):
+        stat, p_value = stats.f_oneway(*self.data_list)
+        if self.tails == 1 and p_value > 0.5:
+            p_value /= 2
+        self.test_name = "ANOVA"
+        self.test_stat = stat
+        self.test_p_value = p_value
+
+    def kruskal_wallis_test(self):
+        stat, p_value = stats.kruskal(*self.data_list)
+        self.test_name = "Kruskal-Wallis test"
+        self.test_stat = stat
+        self.test_p_value = p_value
+
+    def friedman_test(self):
+        stat, p_value = stats.friedmanchisquare(*self.data_list)
+        self.test_name = "Friedman test"
+        self.test_stat = stat
+        self.test_p_value = p_value
+    
+
+
+class __NormalityTests():
+    '''Normality tests mixin'''
 
     def check_normality(self, data):
         # Shapiro-Wilk Test
@@ -45,7 +81,8 @@ class StatisticalAnalysis:
         D_minus = np.max(cdf_values - (np.arange(n) / n))
         D = max(D_plus, D_minus)
 
-        # Use critical values from the Lilliefors table (two-sided test, approximations for large n)
+        # Use critical values from the Lilliefors table 
+        # (two-sided test, approximations for large n)
         critical_values = {
             100: 0.072,
             1000: 0.024,
@@ -58,79 +95,55 @@ class StatisticalAnalysis:
             if n <= size:
                 return D, 1 - critical_value if D < critical_value else 0
 
-        return D, 0
+        return D, 0    
 
-    def t_test(self):
-        if self.paired:
-            t_stat, t_p_value = stats.ttest_rel(self.data_list[0], self.data_list[1])
-        else:
-            t_stat, t_p_value = stats.ttest_ind(self.data_list[0], self.data_list[1])
-        
-        if self.tail == 'one-tailed':
-            t_p_value /= 2
-        
-        self.test_name = "t-test"
-        self.test_stat = t_stat
-        self.test_p_value = t_p_value
 
-        return self._create_result_dict()
+class StatisticalAnalysis(__StatisticalTests, __NormalityTests):
+    '''
+        The main class
+        *documentation placeholder*
+    
+    '''
 
-    def mann_whitney_u_test(self):
-        u_stat, u_p_value = stats.mannwhitneyu(self.data_list[0], self.data_list[1], alternative='two-sided' if self.tail == 'two-tailed' else 'greater')
-        self.test_name = "Mann-Whitney U test"
-        self.test_stat = u_stat
-        self.test_p_value = u_p_value
+    def __init__(self, data_list, paired=False, tails=2):
+        self.data_list = data_list
+        self.paired = paired
+        self.tails = tails
+        self.normals = []
+        self.methods = []
+        self.test_name = None
+        self.test_stat = None
+        self.test_p_value = None
 
-        return self._create_result_dict()
+        # Assertion block
+        assert self.tails in [1,2], "Tails parameter can be 1 or 2 only"
+        assert len(self.data_list) > 1, "At least two groups of data must be given"
+        assert all(len(lst) > 1 for lst in self.data_list), "Each group must contain at least two vaues"        
+        if self.paired == True:
+            assert all(len(lst) == len(self.data_list[0]) for lst in self.data_list), "Paired groups must be the same length"
 
-    def wilcoxon_signed_rank_test(self):
-        w_stat, w_p_value = stats.wilcoxon(self.data_list[0], self.data_list[1])
-        if self.tail == 'one-tailed' and w_p_value > 0.5:
-            w_p_value = 1 - w_p_value
-        self.test_name = "Wilcoxon signed-rank test"
-        self.test_stat = w_stat
-        self.test_p_value = w_p_value
+        for data in data_list:
+            normal, method = self.check_normality(data)
+            self.normals.append(normal)
+            self.methods.append(method)
+            print(f"Data is {'normal' if normal else 'not normal'} (checked by {method})")
 
-        return self._create_result_dict()
+        self.__auto()
+        self.result = self.__create_result_dict()
 
-    def anova(self):
-        f_stat, p_value = stats.f_oneway(*self.data_list)
-        if self.tail == 'one-tailed' and p_value > 0.5:
-            p_value /= 2
-        self.test_name = "ANOVA"
-        self.test_stat = f_stat
-        self.test_p_value = p_value
 
-        return self._create_result_dict()
+    def __auto(self):
+        self.n_groups = len(self.data_list)
+        self.parametric = all(self.normals)
 
-    def kruskal_wallis_test(self):
-        f_stat, p_value = stats.kruskal(*self.data_list)
-        self.test_name = "Kruskal-Wallis test"
-        self.test_stat = f_stat
-        self.test_p_value = p_value
-
-        return self._create_result_dict()
-
-    def friedman_test(self):
-        f_stat, p_value = stats.friedmanchisquare(*self.data_list)
-        self.test_name = "Friedman test"
-        self.test_stat = f_stat
-        self.test_p_value = p_value
-
-        return self._create_result_dict()
-
-    def auto(self):
-        n_groups = len(self.data_list)
-        all_normal = all(self.normals)
-
-        if n_groups == 2:
+        if self.n_groups == 2:
             if self.paired:
-                if all_normal:
+                if self.parametric:
                     return self.t_test()
                 else:
                     return self.wilcoxon_signed_rank_test()
             else:
-                if all_normal:
+                if self.parametric:
                     return self.t_test()
                 else:
                     return self.mann_whitney_u_test()
@@ -138,12 +151,12 @@ class StatisticalAnalysis:
             if self.paired:
                 return self.friedman_test()
             else:
-                if all_normal:
+                if self.parametric:
                     return self.anova()
                 else:
                     return self.kruskal_wallis_test()
 
-    def _get_stars(self):
+    def __make_stars(self):
         if self.test_p_value is not None:
             if self.test_p_value < 0.001:
                 return 3
@@ -154,8 +167,29 @@ class StatisticalAnalysis:
             else:
                 return 0
         return 0
+    
+    def __create_result_dict(self):
 
-    def GetSigmas(self):
+        self.stars_int = self.__make_stars()
+        self.stars_str = '*' * self.stars_int if self.stars_int else 'ns'
+
+        return {
+            "p-value" : self.test_p_value.item(),
+            "TestName" : self.test_name,
+            "N_Groups" : self.n_groups,
+            "ParametricStratistics" : self.parametric,
+            "PairedStratistics" : self.paired,
+            "Tails" : self.tails,
+            "Stars" :  self.stars_int,
+            "StarsPrinted" : self.stars_str,
+            "Statistic": self.test_stat.item(),
+        }
+
+ 
+    def GetResults(self):
+        return self.result
+
+    def GetStats(self):
         if self.test_stat is not None:
             return abs(self.test_stat)
         return None
@@ -167,34 +201,27 @@ class StatisticalAnalysis:
         return self.test_name
 
     def GetStarsInt(self):
-        return self.n_stars
+        return self.stars_int
 
     def GetStarsStr(self):
-        return '*' * self.n_stars if self.n_stars else 'ns'
+        return self.stars_str
 
-    def _create_result_dict(self):
-        return {
-            "TestName": self.test_name,
-            "Statistic": self.test_stat.item(),
-            "p-value": self.test_p_value.item(),
-        }
+
 
 # Example usage
-data1 = np.random.normal(0, 1, 100)
-data2 = np.random.normal(1, 1, 100)
-data3 = np.random.normal(2, 1, 100)
+data = [np.random.normal(i, 1, 100) for i in range(3)]
 
-analysis = StatisticalAnalysis([data1, data2], paired=False, tail='two-tailed')
+analysis = StatisticalAnalysis(data, paired=True, tails=2)
 
 # Running the auto method to automatically decide the test
-results = analysis.auto()
-print(f"{results['TestName']} result: statistic={results['Statistic']}, p-value={results['p-value']}")
+results = analysis.GetResults()
+print(results)
+#print(f"{results['TestName']} result: statistic={results['Statistic']}, p-value={results['p-value']}")
 
 # Accessing separate attributes:
-sigmas = analysis.GetSigmas()
 p_value = analysis.GetP()
 test_name = analysis.GetTestName()
 
-print(f"Number of sigmas: {sigmas}")
+print('')
 print(f"P-value: {p_value}")
 print(f"Test name: {test_name}")
