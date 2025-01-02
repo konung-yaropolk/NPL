ef56d47caf17790caf303ba090caa99d4881ba07
Yaroslav Andrianov <yaropolk1995@gmail.com>
2025-01-02 05:12:18 -0600

    modified:   stats/gui/compile.bat

--- stats/gui/compile.bat
+++ stats/gui/compile.bat
@@ -1,4 +1,4 @@
 echo on
-pyinstaller --name="Statistics" --onefile --windowed --clean --icon=pyc.ico --splash splash.png --optimize 1 --upx-dir upx gui.py
-@REM dont use optimization level 2, it causes ignoring assertions in python code
+pyinstaller --name="Statistics" --onefile --windowed --clean --icon=pyc.ico --splash splash.png --optimize 0 --upx-dir upx gui.py
+@REM dont use optimization, it causes ignoring assertions in python code
 pause
