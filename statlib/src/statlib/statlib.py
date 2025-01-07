aa0c8db6cc4b4a96c95e4ef630456a521b505e50
konung-yaropolk <yaropolk1995@gmail.com>
2025-01-07 16:43:42 -0600

    modified:   statlib/src/statlib/statlib.py

--- statlib/src/statlib/statlib.py
+++ statlib/src/statlib/statlib.py
@@ -280,6 +280,7 @@ class __TextFormatting():
             'Groups_Mean': [np.mean(self.data[i]).item() for i in range(len(self.data))],
             'Groups_SD': [np.std(self.data[i]).item() for i in range(len(self.data))],
             'Groups_SE': [np.std(self.data[i]).item() / np.sqrt(len(self.data)).item() for i in range(len(self.data))],
+            # actually returns list of lists of numpy dtypes of float64, next make it return regular floats:
             'Samples': self.data,
         }
 
